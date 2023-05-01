8ca56a70c57fd5b4c8d33ce28af56b46667d3f9a
anastasiadergaeva <adergaeva@gmail.com>
2023-05-01 20:15:56 +0300

    start homework

---
+++ index.js
@@ -0,0 +1,51 @@
+//1. 
+
+let cleaner = 40000;
+    teacher = 22000;
+console.log(teacher + cleaner);
+
+//2. 
+
+let word = 'Владивосток';
+    n = 2000;
+console.log(word + " " + n);
+
+//3. 
+
+let messenger = 128;
+    likes = 254;
+    vk = 137;
+    yt = 201;
+console.log(messenger + likes + vk + yt);
+
+//4. 
+
+console.log(2 ** 5);
+
+//5. 
+
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+
+//6. 
+
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ', ' + name);
+
+//7. 
+
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.');
+
+//8.
+
+let fahrenheit = 451;
+    celsius = (fahrenheit - 32) / 1.8;
+console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
