00678621c865b10813cb5f57c223f77d8380a3c8
anastasiadergaeva <adergaeva@gmail.com>
2023-10-05 21:33:32 +0300

    fix res

--- index.js
+++ index.js
@@ -1,52 +1,25 @@
-//1. 
+const sum = document.querySelector('.sum').value;
+const button = document.querySelector('.button');
+const total = document.querySelector('.total');
 
-let cleaner = 40000;
-    teacher = 22000;
-console.log(teacher + cleaner);
+const interestRate = 18.5;
 
-//2. 
+const deposit = '30000';
 
-let word = 'Владивосток';
-    n = 2000;
-console.log(word + " " + n);
+const growth = '150';
 
-//3. 
+const currency = 'руб';
 
-let messenger = 128;
-    likes = 254;
-    vk = 137;
-    yt = 201;
-    hrs = (messenger + likes + vk + yt) / 60;
-console.log(hrs);
+let consoleMessage = 'Согласно вашему тарифу, вам была присвоена ставка: ';
 
-//4. 
+console.log(`Ваш депозит на начало расчетного периода составлял ${deposit + " " + currency}`);
+console.log(`${consoleMessage} + ${interestRate}%`);
 
-console.log(2 ** 5);
+console.log(`К концу расчётного периода прирост составил ${growth} рублей и на данный момент ваш депозит составляет ${+deposit + +growth} руб`);
 
-//5. 
-let message = 'Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!';
-console.log(message);
-
-//6. 
-
-const greetings = "Привет";
-const name = "Кот";
-console.log(greetings + ', ' + name);
-
-//7. 
-
-let time;
-time = 34;
-console.log('Старт поездки. Осталось минут: ' + time);
-time -= 15;
-console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
-time -= 10;
-console.log('Немного не новостей, но событий. Осталось минут: ' + time);
-time = 0;
-console.log('Вы приехали. Добро пожаловать в Москву.');
-
-//8.
-
-let fahrenheit = 451;
-    celsius = (fahrenheit - 32) / 1.8;
-console.log(`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
+button.addEventListener('click', () => {
+    const sum = document.querySelector('.sum').value;
+    const result = ((sum / 100) * 7) + +sum;
+    const message = `Через год у вас будет ${result} рублей на счету`
+  total.textContent = (message);
+});
